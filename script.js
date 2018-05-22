1b12584645099051b49f2cf2e7e224c23a05123e
DannyDev <dhernandezm110@gmail.com>
2018-05-22 17:34:34 -0600

    added anchors to social links and content

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
     $(".bio").fadeIn(2000);
     $(".projects").fadeIn(4000);
     $(".work").fadeIn(6000);
-    $(".edu").fadeIn(8000);
-    $(".lang").fadeIn(8000);
-    $(".contact").fadeIn(10000);
+    $(".edu").fadeIn(7000);
+    $(".lang").fadeIn(7000);
+    $(".contact").fadeIn(8000);
 });
