f2c823f05f8bec36dc63265692347470ecff35c2
DannyDev <dhernandezm110@gmail.com>
2018-05-22 14:05:16 -0600

    started to add JS to page

---
+++ script.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+    $(".bio").fadeIn(2000);
+    $(".projects").fadeIn(4000);
+    $(".work").fadeIn(6000);
+    $(".edu").fadeIn(8000);
+    $(".lang").fadeIn(8000);
+    $(".contact").fadeIn(10000);
+});
